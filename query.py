661d0340d7b51c1e0ac64e40dafddad297fcf8e5
SyncopatedPandemonium <kowalczyk.anna01@gmail.com>
2021-10-04 13:10:43 +0200

    Update query.py

--- query.py
+++ query.py
@@ -33,9 +33,10 @@ elif operations == "2":
         if amount_available2 > amount_available1:
             print(f"Maximum availability: {amount_available2} in Warehouse 2.")
         elif amount_available2 < amount_available1:
-            print(f"Borh warehouses have the same amount of item: {amount_available1} in Warehouse 1.")
+            print(f"Maximum availability: {amount_available1} in Warehouse 1.")
         else:
-            print(f"Maximum availability: {amount_available1}.")
+            print(f"Borh warehouses have the same amount of item: {amount_available1}.")
+            
     elif chosen_item in warehouse1:  # chosen item in warehouse 1
         print(f"Location: Warehouse 1")
     elif chosen_item in warehouse2:  # chosen item in warehouse 2
